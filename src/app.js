3a6f196fbff48fd2fccf6bf7f7f00d07159b53f4
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-06-05 11:51:43 +0530

    serving basic html and json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.get("", (req, res) => {
+	res.send("Hello Express!!");
+});
+
+app.get("/about", (req, res) => {
+	res.send("<h1>About Page</h1>");
+});
+
+app.get("/help", (req, res) => {
+	res.send("<h1>Help Page</h1>");
+});
+
+app.get("/data", (req, res) => {
+	res.send({
+		name: "shaik Rehanuddin",
+		age: 21,
+	});
+});
+
 app.listen(3000, () => {
 	console.log("Server Running on port 3000");
 });
