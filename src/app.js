b0e7e268f5ec06aa4d52e231f3980afe8d96547c
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-06-05 12:07:54 +0530

    added dynamic pages

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ const app = express();
 
 const staticDirPath = path.join(__dirname, "../static");
 
+app.set("view engine", "hbs");
 app.use(express.static(staticDirPath));
+
+app.get("", (req, res) => {
+	res.render("index", {
+		title: "Home Page",
+		credits: "Shaik Rehan",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About Page",
+		credits: "Shaik Rehan",
+	});
+});
+
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: "Help Page",
+		credits: "Shaik Rehan",
+	});
+});
 
 app.get("/data", (req, res) => {
 	res.send({
